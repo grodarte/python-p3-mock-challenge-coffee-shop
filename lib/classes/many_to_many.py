8f3695539a02cd3c8732b8d3fee7548315889c31
Gabrielle <ms.grodarte@gmail.com>
2025-01-27 19:18:23 -0800

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,96 @@
 class Coffee:
+
+    all = []
+
     def __init__(self, name):
         self.name = name
+        type(self).all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) >= 3:
+            if not hasattr(self, "_name"):
+                self._name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list(set([order.customer for order in self.orders()]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        prices = [order.price for order in self.orders()]
+        return sum(prices) / len(prices)
 
 class Customer:
+
+    all = []
+
     def __init__(self, name):
         self.name = name
+        type(self).all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) in range(1,16):
+            self._name = name
         
     def orders(self):
-        pass
+        return [order for order in Order.all if self == order.customer]
     
     def coffees(self):
-        pass
+        return list(set([order.coffee for order in self.orders()]))
     
     def create_order(self, coffee, price):
-        pass
+        if isinstance(coffee, Coffee):
+            return Order(self, coffee, price)
+            
     
 class Order:
+
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        type(self).all.append(self)
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, customer):
+        if isinstance(customer, Customer):
+            self._customer = customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, coffee):
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        if isinstance(price, float) and 1.0 <= price <= 10.0:
+            if not hasattr(self, "_price"):
+                self._price = price
